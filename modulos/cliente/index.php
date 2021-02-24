a69912cd3c3f598ef1cae2bdfa712d9aba9eb4eb
Amerson Chagas <amerson.chagas@gmail.com>
2021-02-24 17:17:03 -0300

    Módulos Criados

---
+++ modulos/cliente/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$clientes = array(); 
+
+$clientes[] = array( "nome" => "Joao",  "cpf" => 987987987, "endereco" => "Rua Lua",  "telefone" => 9698426654);
+
+$clientes[] = array( "nome" => "Maria",  "cpf" => 654654654, "endereco" => "Baixada do ambrósio",  "telefone" => 9692326654);
+
+$clientes[] = array( "nome" => "José",  "cpf" => 32164987123, "endereco" => "Ponte do Copala",  "telefone" => 9698426612);
+
+$clientes[] = array( "nome" => "julio",  "cpf" => 3216559874, "endereco" => "Rua do P.Help",  "telefone" => 9698421234);
+
+$clientes[] = array( "nome" => "Marta",  "cpf" => 32132121122, "endereco" => "Av. Fab",  "telefone" => 9691426454);
+
+
+?>
---
+++ modulos/produto/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$produtos = array(); 
+
+$produtos[] = array( "nome" => "Coca-Cola",  "preco" => 8.00, "quantidade" => 50,  "promocao" => false);
+$produtos[] = array( "nome" => "fanta-laranja",  "preco" => 7.00, "quantidade" => 30,  "promocao" => false);
+$produtos[] = array( "nome" => "tuchaua",  "preco" => 4.00, "quantidade" => 150,  "promocao" => true);
+$produtos[] = array( "nome" => "Coca-Cola Zero",  "preco" => 8.00, "quantidade" => 50,  "promocao" => true);
+$produtos[] = array( "nome" => "Guaraná Antartica",  "preco" => 7.00, "quantidade" => 30,  "promocao" => false);
+
+
+?>
